554168ff0ff2690cac26d4d05ace397381d31aab
Lincoln Valdez <lincolnv08@gmail.com>
2016-11-01 12:39:05 -0700

    Still re-formatting

--- players.py
+++ players.py
@@ -1,9 +1,14 @@
-'''This is the file containing all the code needed for each type of blackjack player. It will eventually contain six player types: Dealer, Baseline, Oracle, Hi-Lo, Omega II, and Wong Halves. It currently contains the implementations for the first three.''' 
+'''
+This is the file containing all the code needed for each type of blackjack player. 
+It will eventually contain six player types: Dealer, Baseline, Oracle, Hi-Lo, Omega II, 
+and Wong Halves. It currently contains the implementations for the first three.
+''' 
 from simulator import Game
 
 class Player():
-    '''This is the class that wraps over the functions shared by each of the types of players.'''
-
+    '''
+    This is the class that wraps over the functions shared by each of the types of players.
+    '''
     def __init__(self, playerName):
         self.game = Game()
         self.player = None
@@ -16,13 +21,9 @@ class Player():
             self.player = Oracle(self.game)
     
     def playGame(self):
-<<<<<<< HEAD
         '''
         Plays desired number of hands for given number of iterations.
         '''
-=======
-        '''Plays desired number of hands for given number of iterations.'''
->>>>>>> parent of fc33b0c... Re-formatting docstrings.
         for _ in range(100):
             self.player.hand, self.player.total = self.game.startHand(1)
             print '%s\'s Turn!\n' % self.playerName
@@ -33,7 +34,10 @@ class Player():
             self.getWinnings()
 
     def getWinnings(self):
-        '''Calls getReward() from Game to retrieve the outcome of the hand. Updates total winnings over the whole game.'''
+        '''
+        Calls getReward() from Game to retrieve the outcome of the hand. 
+        Updates total winnings over the whole game.
+        '''
         result = self.game.getReward()
         print '%s\n' % result[0]
         print 'Round Winnings: %s' % result[1]
@@ -43,44 +47,54 @@ class Player():
 
 
 class Dealer():
-    '''The dealer's implementation. Since the dealer's actions are taken care of in the __endHand__ method from Game, this simply outputs the dealer's final hand and total.'''
+    '''
+    The dealer's implementation. Since the dealer's actions are taken care of in the __endHand__ 
+    method from Game, this simply outputs the dealer's final hand and total.
+    '''
     def __init__(self, game):
         self.game = game
 
     def playHand(self):
-<<<<<<< HEAD
         '''
         Outputs dealer's final hand and total for the round.
         '''
-=======
-        '''Outputs dealer's final hand and total for the round.'''
         print 'Dealer\'s Turn!\n'
         print 'Final Hand: %s' % self.game.dealerHand
         print 'Final Total: %s\n' % self.game.dealerTotal
 
 class Baseline():
-    '''The baseline implementation. This player stands on every hand, regardless of the cards.'''
+    '''
+    The baseline implementation. This player stands on every hand, regardless of the cards.
+    '''
     def __init__(self, game):
         self.game = game
         self.hand = []
         self.total = 0
     
     def playHand(self):
-        '''Calls stand() from Game to end the hand. Outputs final hand and total for the round.'''
+        '''
+        Calls stand() from Game to end the hand. Outputs final hand and total for the round.
+        '''
         self.game.stand()
->>>>>>> parent of fc33b0c... Re-formatting docstrings.
         print 'Final Hand: %s' % self.hand
         print 'Final Total: %s\n' % self.total
  
 class Oracle():
-    '''The oracle implementation. This player evaluates all possible actions given the hand and peeks into the deck to make the best possible move for each state, i.e. the perfect blackjack player.'''
+    '''
+    The oracle implementation. This player evaluates all possible actions given the hand 
+    and peeks into the deck to make the best possible move for each state, 
+    i.e. the perfect blackjack player.
+    '''
     def __init__(self, game):
         self.game = game
         self.hand = []
         self.total = 0
     
     def playHand(self):
-        '''Evaluates possible actions and takes actions with the highest reward for the current hand state until the hand is over.'''
+        '''
+        Evaluates possible actions and takes actions with the highest reward for the 
+        current hand state until the hand is over.
+        '''
         action = self.evaluateMoves()
         while action == self.game.hit:
             self.hand, self.total = action()
@@ -96,7 +110,9 @@ class Oracle():
                 print 'Final Total: %s\n' % self.total
         
     def evaluateMoves(self):
-        '''Returns the action with the highest reward given the current hand state.'''
+        '''
+        Returns the action with the highest reward given the current hand state.
+        '''
         moves = self.game.getPossibleActions()
         moveWinnings = []
         if self.game.stand in moves:
@@ -111,11 +127,17 @@ class Oracle():
         else: return max(moveWinnings, key = lambda pair:pair[1])[0]
 
     def standValue(self):
-        '''Future value of standing on the given hand. Calls peekDealer() to see how the hand will compare to the dealer's.'''
+        '''
+        Future value of standing on the given hand. Calls peekDealer() to see how 
+        the hand will compare to the dealer's.
+        '''
         return self.peekDealer(False)
 
     def hitValue(self):
-        '''Future value of hitting on the given hand. Returns the negative bet if the player will bust, otherwise returns 0.'''
+        '''
+        Future value of hitting on the given hand. Returns the negative bet if the 
+        player will bust, otherwise returns 0.
+        '''
         newCard = self.game.deck.peek()
         if self.total + self.game.cardValues[newCard] > 21:
             return -1 * self.game.bet
@@ -123,15 +145,25 @@ class Oracle():
             return 0
     
     def doubleValue(self):
-        '''Future value of doubling down on the given hand. Calls peekDealer() to see how the hand will compare to the dealer's and returns the double the reward.'''
+        '''
+        Future value of doubling down on the given hand. Calls peekDealer() to see how 
+        the hand will compare to the dealer's and returns the double the reward.
+        '''
         return 2 * self.peekDealer(True)
 
     def surrenderValue(self):
-        '''Future value of surrendering on the given hand, which is always losing half the bet.'''
+        '''
+        Future value of surrendering on the given hand, which is always losing half the bet.
+        '''
         return self.game.bet / -2.0
         
     def peekDealer(self, doubling):
-        '''Allows oracle to see into the future and peek at the hand that the dealer will have after taking a given action (either stand or double). Uses doubling flag to indicate where in the deck to start peeking. Returns future reward based on the comparison of player and dealer hands.'''
+        '''
+        Allows oracle to see into the future and peek at the hand that the dealer will have 
+        after taking a given action (either stand or double). Uses doubling flag to indicate 
+        where in the deck to start peeking. Returns future reward based on the comparison 
+        of player and dealer hands.
+        '''
         playerTotal = self.total
         newIndex = -1
         if doubling:
@@ -141,7 +173,10 @@ class Oracle():
         return self.peekReward(playerTotal, dealerTotal)
 
     def getDealerTotal(self, newIndex):
-        '''Plays out the dealer's hand and returns what the dealer's final total would be. Based on __endHand__ from Game.'''
+        '''
+        Plays out the dealer's hand and returns what the dealer's final total would be. 
+        Based on __endHand__ from Game.
+        '''
         dealerTotal = self.game.dealerTotal
         dealerHand = self.game.dealerHand[:]
         while dealerTotal < 17:
@@ -158,7 +193,10 @@ class Oracle():
         return dealerTotal
        
     def peekReward(self, playerTotal, dealerTotal):
-        '''Returns what the player's reward would be based on the comparison of the two hands. Based on getReward() from Game.'''
+        '''
+        Returns what the player's reward would be based on the comparison of the two hands. 
+        Based on getReward() from Game.
+        '''
         if playerTotal > 21:
             return -1 * self.game.bet
         if dealerTotal > 21:
